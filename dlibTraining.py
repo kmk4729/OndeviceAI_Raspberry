210ad9f342c62efb1b1406a869ca8373f5640755
kmk4729 <kmk4729@naver.com>
2024-04-28 20:27:30 +0900

    epoch 100회씩

--- dlibTraining.py
+++ dlibTraining.py
@@ -14,12 +14,8 @@ labels = []
 # 각 폴더에 있는 이미지를 로드하고 레이블을 설정합니다.
 for label in os.listdir(data_dir):
     label_dir = os.path.join(data_dir, label)
-    print(label)
-    print(label_dir)
     for image_name in os.listdir(label_dir):
         image_path = os.path.join(label_dir, image_name)
-        print(image_name)
-        print(image_path)
         # 이미지를 로드하고 크기를 조정합니다. (예: 100x100)
         image = cv2.imread(image_path)
         image = cv2.resize(image, (60, 60))
@@ -31,9 +27,11 @@ for label in os.listdir(data_dir):
 # 이미지와 레이블을 NumPy 배열로 변환합니다.
 images = np.array(images)
 labels = np.array(labels)
-
+images = images / 255.0
 # 데이터를 학습 및 테스트 세트로 분할합니다.
 train_images, test_images, train_labels, test_labels = train_test_split(images, labels, test_size=0.2, random_state=42)
+# 이미지 데이터를 0과 1 사이의 값으로 정규화
+
 
 # CNN 모델 구축
 model = models.Sequential([
@@ -53,7 +51,7 @@ model.compile(optimizer='adam',
               metrics=['accuracy'])
 
 # 모델 학습
-model.fit(train_images, train_labels, epochs=10, validation_data=(test_images, test_labels))
+model.fit(train_images, train_labels, epochs=100, validation_data=(test_images, test_labels))
 
 # 모델 평가
 test_loss, test_accuracy = model.evaluate(test_images, test_labels)
