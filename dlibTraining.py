df7e36e00091c312a7f848018554857e9221f5f3
kmk4729 <kmk4729>
2024-05-01 01:51:11 +0900

    변경사항 저장

--- dlibTraining.py
+++ dlibTraining.py
@@ -3,6 +3,7 @@ import cv2
 import numpy as np
 from sklearn.model_selection import train_test_split
 from tensorflow.keras import layers, models
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 # 이미지가 있는 폴더 경로
 data_dir = "dataset"
@@ -28,12 +29,22 @@ for label in os.listdir(data_dir):
 images = np.array(images)
 labels = np.array(labels)
 images = images / 255.0
+
 # 데이터를 학습 및 테스트 세트로 분할합니다.
 train_images, test_images, train_labels, test_labels = train_test_split(images, labels, test_size=0.2, random_state=42)
-# 이미지 데이터를 0과 1 사이의 값으로 정규화
 
+# 데이터 증강을 위한 ImageDataGenerator 생성
+datagen = ImageDataGenerator(
+    rotation_range=20,      # 회전 각도 범위 (0~180)
+    width_shift_range=0.1,  # 가로 방향 이동 범위 (전체 너비의 비율)
+    height_shift_range=0.1, # 세로 방향 이동 범위 (전체 높이의 비율)
+    shear_range=0.2,        # 전단 강도 범위
+    zoom_range=0.2,         # 확대/축소 범위
+    horizontal_flip=True,   # 수평 뒤집기 여부
+    fill_mode='nearest'     # 이미지를 회전하거나 이동할 때 채울 픽셀 전략
+)
 
-# CNN 모델 구축
+# 모델 구축
 model = models.Sequential([
     layers.Conv2D(32, (3, 3), activation='relu'),
     layers.MaxPooling2D((2, 2)),
@@ -50,13 +61,21 @@ model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-# 모델 학습
-model.fit(train_images, train_labels, epochs=100, validation_data=(test_images, test_labels))
+# 데이터 증강 후 모델 학습
+batch_size = 32
+augmented_train_generator = datagen.flow(train_images, train_labels, batch_size=batch_size)
+
+history = model.fit(
+    augmented_train_generator,
+    steps_per_epoch=train_images.shape[0] // batch_size,
+    epochs=20,
+    validation_data=(test_images, test_labels)
+)
 
 # 모델 평가
 test_loss, test_accuracy = model.evaluate(test_images, test_labels)
 print("Test Accuracy:", test_accuracy)
-model_path = 'my_model/model1.keras'
 
 # 모델 저장
+model_path = 'my_model/model1.keras'
 model.save(model_path)
